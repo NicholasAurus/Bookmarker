fab801968fff52e7dbf3858091e2a90e2a1afc3b
anton <anton@192.168.1.174>
2026-01-14 18:21:57 +0100

    commit

--- src/main/java/it/bookmarker/controller/GestioneUtentiServlet.java
+++ src/main/java/it/bookmarker/controller/GestioneUtentiServlet.java
@@ -10,51 +10,81 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import it.bookmarker.dao.PrestitiDAO;
 import it.bookmarker.dao.UtenteDAO;
+import it.bookmarker.model.Prestito;
 import it.bookmarker.model.Utente;
 
 @WebServlet("/GestioneUtentiServlet")
 public class GestioneUtentiServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-   
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
+        
         
         HttpSession session = request.getSession();
         String ruolo = (String) session.getAttribute("ruoloUtente");
 
-        
         if (ruolo == null || !ruolo.equalsIgnoreCase("BIBLIOTECARIO")) {
             response.sendRedirect("login.jsp"); 
             return;
         }
 
-        UtenteDAO dao = new UtenteDAO();
-        List<Utente> utentiInAttesa = dao.getUtentiInAttesa();
 
+        UtenteDAO utenteDao = new UtenteDAO();
+        PrestitiDAO prestitoDao = new PrestitiDAO();
+
+        
+        List<Utente> utentiInAttesa = utenteDao.getUtentiInAttesa();
         request.setAttribute("listaUtenti", utentiInAttesa);
         
+       
+        List<Prestito> prestitiInAttesa = prestitoDao.getPrestitiRichiesti();
+        request.setAttribute("listaPrestiti", prestitiInAttesa);
+        
+     
         RequestDispatcher dispatcher = request.getRequestDispatcher("approvazioneUtenti.jsp");
         dispatcher.forward(request, response);
     }
 
-    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         
-        String emailUtente = request.getParameter("emailUtente");
-        String azione = request.getParameter("azione");
         
-        UtenteDAO dao = new UtenteDAO();
+        String tipoOperazione = request.getParameter("tipoOperazione"); 
+        String azione = request.getParameter("azione"); 
 
-        if ("accetta".equals(azione)) {
-            dao.updateStato(emailUtente, "attivo");
-        } else if ("rifiuta".equals(azione)) {
-            dao.updateStato(emailUtente, "rifiutato");
+        
+        if ("utente".equals(tipoOperazione)) {
+            String emailUtente = request.getParameter("emailUtente");
+            UtenteDAO dao = new UtenteDAO();
+
+            if ("accetta".equals(azione)) {
+                dao.updateStato(emailUtente, "attivo");
+            } else if ("rifiuta".equals(azione)) {
+                dao.updateStato(emailUtente, "rifiutato");
+            }
+        } 
+       
+        else if ("prestito".equals(tipoOperazione)) {
+           
+            try {
+                int idPrestito = Integer.parseInt(request.getParameter("idPrestito"));
+                PrestitiDAO dao = new PrestitiDAO();
+                
+                if ("accetta".equals(azione)) {
+                  
+                    dao.gestisciPrestito(idPrestito, "attivo");
+                } else if ("rifiuta".equals(azione)) {
+                    dao.gestisciPrestito(idPrestito, "rifiutato");
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
         }
 
-        
+
         response.sendRedirect("GestioneUtentiServlet");
     }
 }
