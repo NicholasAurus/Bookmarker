ce21aebdb04442c5aefeea184e84da228796340a
anton <anton@192.168.1.174>
2026-01-14 17:58:38 +0100

    CORREZIONE

--- src/main/java/it/bookmarker/dao/PrestitiDAO.java
+++ src/main/java/it/bookmarker/dao/PrestitiDAO.java
@@ -11,10 +11,57 @@ import it.bookmarker.model.Prestito;
 public class PrestitiDAO {
     
     
-    private String url = "jdbc:mysql://localhost:3306/biblioteca?serverTimezone=UTC";
-    private String user = "root";
-    private String pass = "Bookmarker09!";
+    private String URL = "jdbc:mysql://localhost:3306/biblioteca?serverTimezone=UTC";
+    private String USER = "root";
+    private String PASS = "Bookmarker09!";
 
+    
+    public List<Prestito> getStoricoByUtente(String emailUtente) {
+        List<Prestito> lista = new ArrayList<>();
+        
+        String sql = "SELECT p.*, l.titolo, l.autore, l.copertina, l.descrizione, " +
+                     "(SELECT COUNT(*) FROM recensioni r WHERE r.libro_id = p.libro_id AND r.utente_email = p.utente_email) as recensioni_count " +
+                     "FROM prestiti p " +
+                     "JOIN libri l ON p.libro_id = l.id_libro " + 
+                     "WHERE p.utente_email = ? " +
+                     "ORDER BY p.data_inizio DESC";
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+                 PreparedStatement ps = conn.prepareStatement(sql)) {
+                
+                ps.setString(1, emailUtente);
+                
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        Prestito p = new Prestito();
+                        
+                        p.setId(rs.getInt("id"));
+                        p.setUtenteEmail(rs.getString("utente_email"));
+                        p.setLibroId(rs.getInt("libro_id"));
+                        p.setDataInizio(rs.getDate("data_inizio"));
+                        p.setDataFinePrevista(rs.getDate("data_fine_prevista"));
+                        p.setDataRestituzioneEffettiva(rs.getDate("data_restituzione_effettiva"));
+                        
+                        p.setTitoloLibro(rs.getString("titolo"));
+                        p.setAutoreLibro(rs.getString("autore"));
+                        p.setCopertinaLibro(rs.getString("copertina"));
+                        p.setDescrizioneLibro(rs.getString("descrizione"));
+                        
+                        p.setRecensito(rs.getInt("recensioni_count") > 0);
+                        
+                        lista.add(p);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+    
+    
     public List<Prestito> getPrestitiRichiesti() {
         List<Prestito> lista = new ArrayList<>();
         
@@ -23,7 +70,7 @@ public class PrestitiDAO {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            try (Connection conn = DriverManager.getConnection(url, user, pass);
+            try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
                  PreparedStatement pstmt = conn.prepareStatement(sql);
                  ResultSet rs = pstmt.executeQuery()) {
 
@@ -52,7 +99,7 @@ public class PrestitiDAO {
         String sql = "UPDATE prestiti SET stato = ? WHERE id = ?";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            try (Connection conn = DriverManager.getConnection(url, user, pass);
+            try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
                  PreparedStatement pstmt = conn.prepareStatement(sql)) {
                 
                 pstmt.setString(1, nuovoStato);
